e7037964a3e713cce0f3b4eadfdd833cff28e605
mohamediderrissi <mohamediderrissi@gmail.com>
2021-02-08 16:40:15 +0100

    a change

--- processes/message.js
+++ processes/message.js
@@ -7,42 +7,43 @@ const processMessage = (event) => {
         const senderID = event.sender.id;
         console.log("Received message from senderId: " + senderID);
         console.log("Message is: " + JSON.stringify(message));
-    // if (message.text == "Comment vas-tu ?") {
-    //   const response = {
-    //     "attachment": {
-    //       "type": "template",
-    //       "payload": {
-    //         "template_type": "generic",
-    //         "elements": [{
-    //           "title": " Très bien et vous ?",
-    //           "subtitle": "Tapper une button pour répondre.",
-    //           "buttons": [
-    //             {
-    //               "type": "postback",
-    //               "title": " Je vais bien, Merci !",
-    //               "payload": "yes",
-    //             },
-    //             {
-    //               "type": "postback",
-    //               "title": "Non, ça ne va pas !",
-    //               "payload": "no",
-    //             }
-    //           ],
-    //         }]
-    //       }
-    //     }
-    //   }
-    //     sendMessage(senderID, response);
-    // }
-    // else if (message.attachments){
-    //   if(message.attachments[0].type == "image")
-    //   {
-    //     sendMessage(senderID, { "text": "Je ne sais pas traiter ce type de demande" });
-    //   }
-    // }
-    // else {
+    if (JSON.stringify(message.text) == "Comment vas-tu ?") {
+      const response = {
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": " Très bien et vous ?",
+              "subtitle": "Tapper une button pour répondre.",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": " Je vais bien, Merci !",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "Non, ça ne va pas !",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+        sendMessage(senderID, response);
+    }
+    else if (message.attachments){
+      if(message.attachments[0].type == "image")
+      {
+        sendMessage(senderID, { "text": "Je ne sais pas traiter ce type de demande" });
+      }
+    }
+    else {
+        // We send back any text inputs  recived from the user  !
         sendMessage(senderID, { "text": message.text });
-    // }
+    }
 }
 };
 module.exports =  processMessage;
