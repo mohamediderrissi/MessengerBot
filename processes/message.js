1c320f575aa0ce5f9aec23c7ac2ff1666a4765ff
mohamediderrissi <mohamediderrissi@gmail.com>
2021-02-08 15:38:41 +0100

    a change

--- processes/message.js
+++ processes/message.js
@@ -8,45 +8,45 @@ const processMessage = (event) => {
         console.log("Received message from senderId: " + senderID);
         console.log("Message is: " + JSON.stringify(message));
         console.log("message.text is: ", message.text);
-    if (message.text == "Comment vas-tu ?") {
-        // senderAction(senderID);
-        const messageWithOptions = {
-                "text": "Très bien et vous ?",
-                "quick_replies":[
-                  {
-                    "content_type":"text",
-                    "title":"Je vais bien, merci ",
-                  },{
-                    "content_type":"text",
-                    "title":"Non, ça ne va pas ",
-                  }
-                ]
-        };
-        sendMessage(senderID, messageWithOptions);
-    }
-    else if (message.attachments){
-      if(message.attachments[0].type == "image")
-      {
-        senderAction(senderID);
-        sendMessage(senderID, { "text": "Je ne sais pas traiter ce type de demande" });
-        const messageWithOptions = {
-          "text": "Très bien et vous ?",
-          "quick_replies":[
-            {
-              "content_type":"text",
-              "title":"Je vais bien, merci ",
-            },{
-              "content_type":"text",
-              "title":"Non, ça ne va pas ",
-            }
-          ]
-        };
-        sendMessage(senderID, messageWithOptions);
-      }
-    }
-    else {
+    // if (message.text == "Comment vas-tu ?") {
+    //     // senderAction(senderID);
+    //     const messageWithOptions = {
+    //             "text": "Très bien et vous ?",
+    //             "quick_replies":[
+    //               {
+    //                 "content_type":"text",
+    //                 "title":"Je vais bien, merci ",
+    //               },{
+    //                 "content_type":"text",
+    //                 "title":"Non, ça ne va pas ",
+    //               }
+    //             ]
+    //     };
+    //     sendMessage(senderID, messageWithOptions);
+    // }
+    // else if (message.attachments){
+    //   if(message.attachments[0].type == "image")
+    //   {
+    //     senderAction(senderID);
+    //     sendMessage(senderID, { "text": "Je ne sais pas traiter ce type de demande" });
+    //     const messageWithOptions = {
+    //       "text": "Très bien et vous ?",
+    //       "quick_replies":[
+    //         {
+    //           "content_type":"text",
+    //           "title":"Je vais bien, merci ",
+    //         },{
+    //           "content_type":"text",
+    //           "title":"Non, ça ne va pas ",
+    //         }
+    //       ]
+    //     };
+    //     sendMessage(senderID, messageWithOptions);
+    //   }
+    // }
+    // else {
         sendMessage(senderID, message);
-    }
+    // }
 }
 };
 module.exports =  processMessage;
